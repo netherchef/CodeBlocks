197a922b2aecf502a5e222e1bb2be03abff6f4a6
God <netherchef@gmail.com>
2019-02-18 14:46:37 +0800

    Added [Unity Project Folder]

---
+++ Code Blocks/Assets/Scripts/EnumeratorTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumeratorTracker : MonoBehaviour {
+
+	public static int runningEnums;
+	public int runningEnumerators;
+
+	private void Update () {
+		runningEnumerators = runningEnums;
+	}
+
+	public static void AddEnumFlag () {
+		runningEnums++;
+	}
+
+	public static void RemoveEnumFlag () {
+		runningEnums--;
+	}
+}
