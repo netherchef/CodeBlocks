5638ba5aa27b83ca8098ccaa34187fb828ce749e
Nomad <netherchef@gmail.com>
2019-08-11 15:01:07 +0800

    Update PhysicsTools.cs

--- PhysicsTools.cs
+++ PhysicsTools.cs
@@ -4,9 +4,47 @@ public class PhysicsTools
 {
 	public class Gravity
 	{
-		public static void AlterSpeed (float currDist, float fullDist, float moveSpeed, out float currSpeed)
+		public class Alter_Speed
 		{
-			currSpeed = currDist / fullDist * moveSpeed;
+			public static void By_MinDistance (float currDist, float fullDist, float minDist, float fullSpeed, out float currSpeed_out, float minSpeed)
+			{
+				if (currDist > minDist)
+				{
+					Alter_Speed_Relative_Distance (currDist, fullDist, fullSpeed, out currSpeed_out);
+				}
+				else
+				{
+					currSpeed_out = minSpeed;
+				}
+			}
+
+			public static void By_MinSpeed_And_MinDistance (float currDist, float fullDist, float minDist, float fullSpeed, in float currSpeed_in, out float currSpeed_out, float minSpeed)
+			{
+				if (currSpeed_in > minSpeed)
+				{
+					Alter_Speed_Relative_Distance (currDist, fullDist, fullSpeed, out currSpeed_out);
+				}
+				else if (currDist < minDist)
+				{
+					currSpeed_out = minSpeed;
+				}
+				else
+				{
+					Alter_Speed_Relative_Distance (currDist, fullDist, fullSpeed, out currSpeed_out);
+				}
+			}
+
+			public static void Alter_Speed_Relative_Distance (float currDist, float fullDist, float fullSpeed, out float currSpeed)
+			{
+				if (currDist <= fullDist)
+				{
+					currSpeed = currDist / fullDist * fullSpeed;
+				}
+				else
+				{
+					currSpeed = fullSpeed;
+				}
+			}
 		}
 	}
 }
