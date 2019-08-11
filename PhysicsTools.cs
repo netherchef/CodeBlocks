d4d20e7962a0abba7a952c903e3f3dae7e798d35
Nomad <netherchef@gmail.com>
2019-08-11 14:21:13 +0800

    Create PhysicsTools.cs

---
+++ PhysicsTools.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PhysicsTools
+{
+	public class Gravity
+	{
+		public static void AlterSpeed (float currDist, float fullDist, float moveSpeed, out float currSpeed)
+		{
+			currSpeed = currDist / fullDist * moveSpeed;
+		}
+	}
+}
