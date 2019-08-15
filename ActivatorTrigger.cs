b5d59a19394888fc51f7a81c8e9e4e5fc15addb8
Nomad <netherchef@gmail.com>
2019-08-16 07:29:50 +0800

    Create ActivatorTrigger.cs

---
+++ ActivatorTrigger.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public enum ActivatorType { OnThenOff, OffThenOn }
+
+public class ActivatorTrigger : MonoBehaviour
+{
+	// Activator Trigger Variables
+
+	[Header ("Activator Type:")]
+	public ActivatorType activatorType;
+
+	[Header ("Target Tag:")]
+	public string targetTag;
+
+	[Header ("Target:")]
+	public GameObject[] objects;
+
+	private void OnTriggerEnter2D (Collider2D collision)
+	{
+		if (collision.CompareTag (targetTag))
+		{
+			Select_Entry_Action ();
+		}
+	}
+
+	private void OnTriggerExit2D (Collider2D collision)
+	{
+		if (collision.CompareTag (targetTag))
+		{
+			Select_Exit_Action ();
+		}
+	}
+
+	void Select_Entry_Action ()
+	{
+		if (activatorType == ActivatorType.OnThenOff) OnOff (true);
+		else if (activatorType == ActivatorType.OffThenOn) OnOff (false);
+	}
+
+	void Select_Exit_Action ()
+	{
+		if (activatorType == ActivatorType.OnThenOff) OnOff (false);
+		else if (activatorType == ActivatorType.OffThenOn) OnOff (true);
+	}
+
+	void OnOff (bool state)
+	{
+		for (int e = 0; e < objects.Length; e++)
+		{
+			objects[e].SetActive (state);
+		}
+	}
+}
