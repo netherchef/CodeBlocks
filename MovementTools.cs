bf0073d676634e565adcd9f6f10cdb4845c87211
God <netherchef@gmail.com>
2019-08-17 01:04:59 +0800

    Update MovementTools.cs

--- MovementTools.cs
+++ MovementTools.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementTools
 {
     public class Velocity
     {
-        public static void Move_X (Rigidbody2D rb2d, int direction, float speed)
+        public static void Move_X (Rigidbody2D rb2d, float direction, float speed)
         {
             rb2d.velocity = new Vector2 (direction * speed, 0) * Time.deltaTime;
         }
@@ -19,19 +18,53 @@ public class MovementTools
 
     public class Translate
     {
-        public static void Move_X (Transform trans, int direction, float speed)
+        public class Variable_Speed
         {
-            trans.Translate (new Vector2 (direction * speed, 0) * Time.deltaTime);
+            public static void Move_X (Transform trans, float direction, float speed)
+            {
+                trans.Translate (new Vector2 (direction * speed, 0) * Time.deltaTime);
+            }
+
+            public static void Move_Y (Transform trans, float direction, float speed)
+            {
+                trans.Translate (new Vector2 (0, direction * speed) * Time.deltaTime);
+            }
+
+            public static void Move_XY (Transform trans, float directionX, float directionY, float speed)
+            {
+                trans.Translate (new Vector2 (directionX, directionY) * speed * Time.deltaTime);
+            }
+        }
+    }
+
+    public class Shift
+    {
+        public static IEnumerator X_To (float targetPos, Transform targetObject, float speed)
+        {
+            while (Mathf.Abs (targetObject.position.x - targetPos) > 0.1f)
+            {
+                float direction = -(Mathf.Sign (targetObject.position.x - targetPos));
+
+                Translate.Variable_Speed.Move_X (targetObject, direction, speed);
+
+                yield return null;
+            }
+
+            targetObject.position = new Vector3 (targetPos, targetObject.position.y, targetObject.position.z);
         }
 
-        public static void Move_Y (Transform trans, int direction, float speed)
+        public static IEnumerator Y_To (float targetPos, Transform targetObject, float speed)
         {
-            trans.Translate (new Vector2 (0, direction * speed) * Time.deltaTime);
-        }
+            while (Mathf.Abs (targetObject.position.y - targetPos) > 0.1f)
+            {
+                float direction = -(Mathf.Sign (targetObject.position.y - targetPos));
 
-        public static void Move_XY (Transform trans, int directionX, int directionY, float speed)
-        {
-            trans.Translate (new Vector2 (directionX, directionY) * speed * Time.deltaTime);
+                Translate.Variable_Speed.Move_Y (targetObject, direction, speed);
+
+                yield return null;
+            }
+
+            targetObject.position = new Vector3 (targetObject.position.x, targetPos, targetObject.position.z);
         }
     }
 }
